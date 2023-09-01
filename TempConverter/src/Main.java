810c779877564a7f208e903259cd2811c2907275
Jadan Kerr <badi1863@gmail.com>
2023-09-01 17:52:13 -0600

    Wrote things

--- TempConverter/src/Main.java
+++ TempConverter/src/Main.java
@@ -4,28 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         TempConvert tempConvert = new TempConvert();
-
+        //Prompts the user for a temperature conversion direction
         System.out.println("Which way are you converting?");
         System.out.println("A) Celsius > Fahrenheit \nB) Fahrenheit > Celsius");
 
         String response = myObj.nextLine();
         String typeSelection;
         double inputNum;
-
+        //If user response A, the program uses the C>F conversion. Vice versa if B
+        String unit;
+        String otherUnit;
         if (response.equalsIgnoreCase("A")){
             typeSelection = "A";
-            System.out.println("Enter the value (C) to be converted:");
-
+             unit = "°Celsius";
 
         }
         else {
             typeSelection = "B";
+            unit = "°Fahrenheit";
             System.out.println("Enter the value (F) to be converted:");
 
 
         }
         inputNum = Double.parseDouble(myObj.nextLine());
-        System.out.println(tempConvert.convert(inputNum, typeSelection));
+        //Print output
+        if(typeSelection.equals("A")) {
+            otherUnit = "°F";
+        }
+        else {
+            otherUnit = "°C";
+        }
+        System.out.println("Your input value: "+ inputNum +" "+ unit + "\nCalculates to: " + tempConvert.convert(inputNum, typeSelection) +" " + otherUnit);
 
     }
 }
--- TempConverter/src/TempConvert.java
+++ TempConverter/src/TempConvert.java
@@ -1,10 +1,11 @@
 public class TempConvert {
     public double convert(double inputInt, String typeSelection) {
         double output;
+        //Uses the correct formula according to User selection
         if (typeSelection.equals("A")) {
             output = (inputInt * 9 / 5) + 32;
         } else {
-            output = (inputInt * 5 / 9) - 32;
+            output = (inputInt - 32) * 5/9;
 
         }
         return output;
